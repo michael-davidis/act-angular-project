c12674ae51cdc57402407973e6a6c67990fe6acc
Michael Davidis <mdavidis@cluster.gr>
2022-01-04 15:17:25 +0200

    Created devices' basic functionality

--- final-project-angular/src/app/components/smartphones/smartphones.component.ts
+++ final-project-angular/src/app/components/smartphones/smartphones.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Device } from 'src/app/models/Device';
+import { DeviceManagementService } from 'src/app/services/device-management.service';
 
 @Component({
   selector: 'app-smartphones',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./smartphones.component.css']
 })
 export class SmartphonesComponent implements OnInit {
+  smartphones: Device[] = [];
 
-  constructor() { }
+  constructor( private router: Router, private deviveManagementService: DeviceManagementService ) { }
 
   ngOnInit(): void {
+    this.smartphones = this.deviveManagementService.devices.filter( device => device.type == 1);
+  }
+
+  viewSmartphone( id: string){
+    this.router.navigate(['smartphones', id])
   }
 
 }
--- final-project-angular/src/app/components/tablets/tablets.component.ts
+++ final-project-angular/src/app/components/tablets/tablets.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Device } from 'src/app/models/Device';
+import { DeviceManagementService } from 'src/app/services/device-management.service';
 
 @Component({
   selector: 'app-tablets',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tablets.component.css']
 })
 export class TabletsComponent implements OnInit {
+  tablets: Device[] = [];
 
-  constructor() { }
+  constructor( private router: Router, private deviveManagementService: DeviceManagementService ) { }
 
   ngOnInit(): void {
+    this.tablets = this.deviveManagementService.devices.filter( device => device.type == 2 );
+  }
+
+  viewTablet( id: string){
+    this.router.navigate(['tablets', id])
   }
 
 }
