77a558b13ef53fe60a785dfc1b992cd6393e5c7d
Michael Davidis <mdavidis@cluster.gr>
2022-01-04 09:40:31 +0200

    Updated app main module

--- final-project-angular/src/app/app.module.ts
+++ final-project-angular/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppComponent } from './app.component';
 import { EmployeesComponent } from './components/employees/employees.component';
 import { RouterModule, Routes } from '@angular/router';
 import { DevicesComponent } from './components/devices/devices.component';
+import { SmartphonesComponent } from './components/smartphones/smartphones.component';
+import { TabletsComponent } from './components/tablets/tablets.component';
+import { HomeComponent } from './components/home/home.component';
+import { EmployeeDetailsComponent } from './components/employee-details/employee-details.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CreateEmployeeComponent } from './components/create-employee/create-employee.component';
+import { CreateDeviceComponent } from './components/create-device/create-device.component';
+
 
 
 const appRoutes: Routes = [
   {
-    path: 'home', // or empty, it's the same
-    component: AppComponent
+    path: 'home',
+    component: HomeComponent,
   },
   {
     path: 'employees',
@@ -21,19 +29,39 @@ const appRoutes: Routes = [
     component: DevicesComponent,
   },
   {
-    path: 'employees/:id',
-    component: DevicesComponent,
+    path: 'smartphones',
+    component: SmartphonesComponent,
   },
   {
-    path: 'devices/:id',
-    component: DevicesComponent,
+    path: 'tablets',
+    component: TabletsComponent,
+  },
+  {
+    path: 'employees/:id',
+    component: EmployeeDetailsComponent,
+  },
+  {
+    path: 'smartphones/:id',
+    component: SmartphonesComponent,
+  },
+  {
+    path: 'tablets/:id',
+    component: TabletsComponent,
+  },
+  {
+    path: 'createEmployee',
+    component: CreateEmployeeComponent,
+  },
+  {
+    path: 'createDevice',
+    component: CreateDeviceComponent,
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
-  { // Sends to if there is something wrong with the path
+  {
     path: '**',
     redirectTo: 'home',
     pathMatch: 'full'
@@ -42,12 +70,20 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeesComponent
+    EmployeesComponent,
+    DevicesComponent,
+    SmartphonesComponent,
+    TabletsComponent,
+    HomeComponent,
+    EmployeeDetailsComponent,
+    CreateEmployeeComponent,
+    CreateDeviceComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
