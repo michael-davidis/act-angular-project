3bf7c19f47ed8e5d59e0c1117673c60bdb19fd6d
Michael Davidis <mdavidis@cluster.gr>
2022-01-04 09:38:18 +0200

    Added new functions, updated existing ones and placed placeholder functions

--- final-project-angular/src/app/services/employee-management.service.ts
+++ final-project-angular/src/app/services/employee-management.service.ts
@@ -7,30 +7,48 @@ import { Employee } from '../models/Employee';
 })
 export class EmployeeManagementService {
   employees: Employee[] = [];
+  employeesIDs: any[] = [];
 
   constructor(private http: HttpClient) {
-    // this.getEmployees();
+    // this.employees = [new Employee(1, "Michael", "michael@gmail.com"), new Employee(2, "John", "john@gmail.com"), new Employee(3, "George", "george@gmail.com")];
+    // for (let employee of this.employees) {
+    //   this.http.post("https://act-final-project-default-rtdb.europe-west1.firebasedatabase.app/Employees.json", employee).subscribe(() => {
+    //   });
+    // }
+    this.getEmployeesData();
   }
 
-  getEmployees(): void {
-    // this.http
-    //   .get(
-    //     'https://typescript-ddaddefault-rtdb.firebaseio.com/Employees.json'
-    //   )
-    //   .subscribe((response: any) => {
-    //     for (let key in response) {
-    //       this.employees.push(
-    //         new Employee(key, response[key].name, response[key].email)
-    //       );
-    //     }
-    //   });
+  createEmployee(employee: Employee){
+      this.http.post("https://act-final-project-default-rtdb.europe-west1.firebasedatabase.app/Employees.json", employee).subscribe((response) => {
+        this.getEmployeesData();
+      });
+  }
+
+  getEmployeesData(): void {
+    this.http
+      .get(
+        'https://act-final-project-default-rtdb.europe-west1.firebasedatabase.app/Employees.json'
+      )
+      .subscribe((response: any) => {
+        for (let key in response) {
+          this.employees.push(
+            new Employee(
+              response[key].id,
+              response[key].name,
+              response[key].email
+            )
+          );
+
+          this.employeesIDs[response[key].id] = key + '';
+        }
+      });
   }
 
   editEmployee(employee: Employee) {
     // this.http
     //   .put(
     //     'https://typescript-dwadaw-default-rtdb.firebaseio.com/Employees/' +
-    //       employee.id +
+    //       this.employeesIDs[employee.id] +
     //       '.json',
     //     {
     //       id: employee.id,
@@ -43,7 +61,6 @@ export class EmployeeManagementService {
     //     let index = this.employees.findIndex((el) => {
     //       return (el.id = newEmp.id);
     //     });
-
     //     this.employees[index] = newEmp;
     //   });
   }
