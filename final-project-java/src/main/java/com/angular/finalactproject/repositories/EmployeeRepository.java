41a16c8aa3eb0f8d6cabfe1828f1e17b1e24230b
Michael Davidis <mdavidis@cluster.gr>
2022-01-05 18:44:43 +0200

    Removed unsed custom backend

--- final-project-java/src/main/java/com/angular/finalactproject/repositories/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.angular.finalactproject.repositories;
-
-import com.angular.finalactproject.entities.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
--- final-project-java/src/main/java/com/angular/finalactproject/services/EmployeeDevicesService.java
+++
@@ -1,9 +0,0 @@
-package com.angular.finalactproject.services;
-
-import com.angular.finalactproject.entities.EmployeeDevices;
-
-public interface EmployeeDevicesService {
-
-    EmployeeDevices saveDeviceToEmployee(EmployeeDevices eDevice);
-
-}
